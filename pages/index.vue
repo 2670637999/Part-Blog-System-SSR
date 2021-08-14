8ed7abc5423ae64208c595402a3554401a184b52
陈陈菌 <2670637999@qq.com>
2021-08-14 18:12:20 +0800

    更新

--- pages/index.vue
+++ pages/index.vue
@@ -65,7 +65,7 @@
                 <!-- 网易云音乐外部播放器 -->
                 <ul id="music">
                     <h3>音乐</h3>
-                    <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=1491212&auto=0&height=66"></iframe>
+                    <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=19934760&auto=1&height=66"></iframe>
                 </ul>
                 <!-- 随机文章组件 -->
                 <transition mode="in-out" enter-active-class="part-enter-13" leave-active-class="part-leave-1">
